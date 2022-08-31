28d8029c3ed5921eb52fcf0962552d6e9d9c8e02
Semih Dağhan <daghanex@hotmail.com>
2022-08-31 23:36:33 +0300

    JSX Formatı içerisinde Javascript kullanımı

--- React Project/ReactEgitimSerisi/reactapp/src/App.js
+++ React Project/ReactEgitimSerisi/reactapp/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const test = 34;
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{1+1}</h1>
+      <h2>{"semih".toUpperCase()}</h2>
+      <h3>{test}</h3>
     </div>
   );
 }
