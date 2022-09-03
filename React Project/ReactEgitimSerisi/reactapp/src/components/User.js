4c9d3b15e4aff5283428173049388961cbe5e1c0
Semih Dağhan <daghanex@hotmail.com>
2022-09-03 23:56:21 +0300

    User arayüzü ve font awesome cdn eklenmesi

--- React Project/ReactEgitimSerisi/reactapp/src/components/User.js
+++ React Project/ReactEgitimSerisi/reactapp/src/components/User.js
@@ -5,12 +5,17 @@ class User extends Component {
     // Destructing
     const {name,department,salary} = this.props;
     return (
-      <div>
-          <ul>
-            <li>İsim : {name}</li>
-            <li>Departman : {department}</li>
-            <li>Maaş : {salary}</li>
-          </ul>
+      <div className='col-md-8 mb-4'>
+        <div className='card'>
+          <div className='card-header d-flex justify-content-between'>
+            <h4 className='d-inline'>{name} </h4>
+            <i className="fa-solid fa-trash-can" style={{cursor:"pointer"}}></i>
+          </div>
+          <div className='card-body'>
+            <p className='card-text'>Maaş : {salary}</p>
+            <p className='card-text'>Department : {department}</p>
+          </div>
+        </div>
       </div>
     )
   }
