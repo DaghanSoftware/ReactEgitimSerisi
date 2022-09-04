d4e9c55b99430addca5f80429775b89f8b971303
Semih Dağhan <daghanex@hotmail.com>
2022-09-04 21:12:22 +0300

    Bind ile değer gönderme
    Bind ile değer göndermek için this, göndermek istediğimiz değeri yazmamız gerekmektedir.
    Method'da ise gönderdiğimiz değeri yakalamak için ilk önce değeri tutacak bir parametre sonra event parametresini yazmalıyız.

--- React Project/ReactEgitimSerisi/reactapp/src/components/User.js
+++ React Project/ReactEgitimSerisi/reactapp/src/components/User.js
@@ -7,8 +7,8 @@ class User extends Component {
       isVisible: false
     }
   }
-  onClickEvent = (e) =>{
-    console.log(this);
+  onClickEvent = (number,e) =>{
+    console.log(number);
   }
   render() {
     // Destructing
@@ -18,7 +18,7 @@ class User extends Component {
       <div className='col-md-8 mb-4'>
         <div className='card'>
           <div className='card-header d-flex justify-content-between'>
-            <h4 className='d-inline' onClick={this.onClickEvent}>{name} </h4>
+            <h4 className='d-inline' onClick={this.onClickEvent.bind(this,34)}>{name} </h4>
             <i className="fa-solid fa-trash-can" style={{cursor:"pointer"}}></i>
           </div>
           {
