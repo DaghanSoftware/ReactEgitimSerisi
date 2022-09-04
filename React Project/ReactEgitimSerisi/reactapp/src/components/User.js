e4dbad728e496aee76b73a0121f9c755144fb995
Semih Dağhan <daghanex@hotmail.com>
2022-09-05 00:37:24 +0300

    "setState" stateleri değiştirme
    Componentin statesini değiştirmek için kullandığımız method.

--- React Project/ReactEgitimSerisi/reactapp/src/components/User.js
+++ React Project/ReactEgitimSerisi/reactapp/src/components/User.js
@@ -7,8 +7,10 @@ class User extends Component {
       isVisible: false
     }
   }
-  onClickEvent = (number,e) =>{
-    console.log(number);
+  onClickEvent = (e) =>{
+    this.setState({
+      isVisible: !this.state.isVisible
+    })
   }
   render() {
     // Destructing
@@ -18,7 +20,7 @@ class User extends Component {
       <div className='col-md-8 mb-4'>
         <div className='card'>
           <div className='card-header d-flex justify-content-between'>
-            <h4 className='d-inline' onClick={this.onClickEvent.bind(this,34)}>{name} </h4>
+            <h4 className='d-inline' onClick={this.onClickEvent}>{name} </h4>
             <i className="fa-solid fa-trash-can" style={{cursor:"pointer"}}></i>
           </div>
           {
