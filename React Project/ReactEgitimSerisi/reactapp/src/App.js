0f555c75fbdd6a016b92eb3fe4bfd1b1fb209565
Semih Dağhan <daghanex@hotmail.com>
2022-09-05 17:39:11 +0300

    Componentler Arası İletişim (Props Drilling)

--- React Project/ReactEgitimSerisi/reactapp/src/App.js
+++ React Project/ReactEgitimSerisi/reactapp/src/App.js
@@ -1,19 +1,40 @@
+import { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import User from './components/User';
-function App() {
+import Users from './components/Users';
+class App extends Component {
+  state = {
+    users: [
+      {
+        id: 1,
+        name: "Semih Dağhan",
+        salary : "5000",
+        department : "Bilişim"
+      },
+      {
+        id: 2,
+        name: "Emirhan Dağhan",
+        salary : "6000",
+        department : "Tamirci"
+      },
+      {
+        id: 3,
+        name: "Burak Alak",
+        salary : "2500",
+        department : "Öğrenci"
+      }
+    ]
+  }
+  render(){
   return (
     
     <div className="container">
       <Navbar title = "User App"/>
       <hr></hr>
-      <User
-      name ="Semih Dağhan"
-      salary = "5000"
-      department = "Bilişim"
-      />
+      <Users users = {this.state.users}/>
     </div>
-  );
+  )
+  }
 }
 
 export default App;
