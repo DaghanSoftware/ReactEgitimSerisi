c98d5758af8e02e917bd29e69c2ad21c21d33187
Semih Dağhan <daghanex@hotmail.com>
2022-09-11 23:41:03 +0300

    action reducer dispatch kullanımı
    silme işleminde sorun

--- React Project/ReactEgitimSerisi/reactapp/src/components/User.js
+++ React Project/ReactEgitimSerisi/reactapp/src/components/User.js
@@ -1,66 +1,61 @@
 import React, { Component } from 'react'
-import UserConsumer from '../context';
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
+import UserConsumer from '../context'
 class User extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      isVisible: false
-    }
+  state = {
+    isVisible : false
   }
-  onClickEvent = (e) =>{
+  onClickEvent = (e) => {
     this.setState({
       isVisible: !this.state.isVisible
     })
   }
-  onDeleteUser = (dispatch,e) => {
-      const {id} = this.props;
-      dispatch({type:"DELETE_USER",payload:id});
+  onDeleteUser= (dispatch,e) =>{
+    console.log("denem");
+    const {id} = this.props;
+    dispatch({type:"DELETE_USER",payload:id});
   }
-  render() {
     // Destructing
+    render(){
     const {name,department,salary} = this.props;
     const {isVisible} = this.state;
     return (
     <UserConsumer>
-      {
-        value => {
-          const {dispatch} = value;
-
-              return (
+        {
+          value => {
+            const {dispatch} = value;
+                return (
                 <div className='col-md-8 mb-4'>
                   <div className='card'>
                     <div className='card-header d-flex justify-content-between'>
                       <h4 className='d-inline' onClick={this.onClickEvent}>{name} </h4>
-                      <i className="fa-solid fa-trash-can" onClick={this.onDeleteUser.bind(this.dispatch)} style={{cursor:"pointer"}}></i>
+                      <i onClick={this.onDeleteUser.bind(this,dispatch)} className="fa-solid fa-trash-can" ></i>
                     </div>
                     {
                       isVisible ? <div className='card-body'>
                       <p className='card-text'>Maaş : {salary}</p>
                       <p className='card-text'>Department : {department}</p>
-                      
+
                     </div> : null
                     }
-                    
+
                   </div>
                 </div>
               )
-            
+          }
         }
-      }
     </UserConsumer>)
 
+  }
 }
-
-// User.defaultProps = {
-//   name : "Bilgi Yok",
-//   salary : "Bilgi Yok",
-//   department : "Bilgi yok"
-// }
-// User.propTypes = {
-//   name : PropTypes.string.isRequired,
-//   salary : PropTypes.string.isRequired,
-//   department: PropTypes.string.isRequired
-// }
+User.defaultProps = {
+  name : "Bilgi Yok",
+  salary : "Bilgi Yok",
+  department : "Bilgi yok"
+}
+User.propTypes = {
+  name : PropTypes.string.isRequired,
+  salary : PropTypes.string.isRequired,
+  department: PropTypes.string.isRequired
 }
 export default User;
